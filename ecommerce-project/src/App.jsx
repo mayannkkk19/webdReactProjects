4fa665eafd5136688e4e0c475907082b883ad958
mayankyadav19 <hatmario06@gmail.com>
2026-03-24 21:58:47 +0530

    generate-html-for-cart-items

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -3,14 +3,14 @@ import { CheckoutPage } from "./pages/CheckoutPage";
 import { OrdersPage } from "./pages/OrdersPage";
 import { TrackingPage } from "./pages/TrackingPage";
 import { useState, useEffect} from "react";
-import { axios } from 'axios';
+import  axios  from 'axios';
 import { Routes, Route } from "react-router";
 import "./App.css";
 
 function App() {
   const [cart, setCart] = useState([]);
   useEffect(() => {
-    axios.get("http://localhost:3000/api/cart-items").then((response) => {
+    axios.get('/api/cart-items?expand=product').then((response) => {
       setCart(response.data);
     });
   }, []);
